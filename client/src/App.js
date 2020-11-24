958bea69bfe5d19ea811ce4baae59a99505f39ef
n.pankov <pankoff2000@gmail.com>
2020-11-24 17:47:48 +0100

    step 4. add pages, routes, state auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { useRoutes } from './routes';
+import { BrowserRouter as Router } from 'react-router-dom';
+import 'materialize-css';
+
+
 
 function App() {
+  const routes = useRoutes(false);
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="container">
+      <Router>
+        {routes}
+      </Router>
     </div>
   );
 }
